cde334d2cf24f7bd877b18706420ed6c0db4979b
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-27 22:47:57 +0200

    Add files via upload

---
+++ 07.SpecialBonus/Program.cs
@@ -0,0 +1,23 @@
+﻿int stopNum = int.Parse(Console.ReadLine());
+int prevNum = 0;
+
+while (true)
+
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num == stopNum)
+    {
+        break;
+
+    }
+
+    prevNum = num;
+
+
+
+}
+Console.WriteLine(prevNum * 1.2);
+
+
+
---
+++ 08. AccountBalance/Program.cs
@@ -0,0 +1,30 @@
+﻿double balance = 0;
+string end;
+
+while ((end = Console.ReadLine()) != "End")
+
+{
+    double money = double.Parse(end);
+    balance += money;
+
+    if (money >= 0)
+    {
+        Console.WriteLine($"Increase: {money:F2}");
+
+    }
+    else
+    {
+        Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+
+
+    }
+
+
+
+}
+
+Console.WriteLine($"Balance: {balance:F2}");
+
+
+
+
