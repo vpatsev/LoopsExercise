a101b3ea3339a9e51b6b36b5e3ba206f5f32b1b0
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-27 22:44:49 +0200

    Add files via upload

---
+++ 01.PowerOfNums/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+int result = n;
+
+for (int i = 1; i < p; i++)
+
+{
+    result *= n;
+
+}
+Console.WriteLine(result);
---
+++ 03.BiggestNum/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int biggestN = int.MinValue;
+
+for (int i = 0; i < num; i++)
+{
+    int currentN = int.Parse(Console.ReadLine());
+    if (currentN > biggestN)
+    {
+        biggestN = currentN;
+    }
+}
+Console.WriteLine(biggestN);
